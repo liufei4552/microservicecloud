960da9bab3b621a1cd76f281ac6a8671cf466ced
liufei4552 <304265966@qq.com>
2020-08-27 11:15:28 +0800

    添加zuul路由网关模块

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # microservicecloud
 ##springcloud学习笔记
 ###备注
-####1、为了在一台机器上启动eureka集群C:\Windows\System32\drivers\etc 中添加127.0.0.1 eureka7001.com 127.0.0.1 eureka7002.com 127.0.0.1 eureka7003.com
+####1、为了在一台机器上启动eureka集群C:\Windows\System32\drivers\etc 中添加127.0.0.1 eureka7001.com 127.0.0.1 eureka7002.com 127.0.0.1 eureka7003.com 127.0.0.1 gateway-9527.com
 ####2、Zookeeper和Eureka区别，前者使用cp（关注数据一致性）后者使用ap（关注服务可用）
---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/atguigu/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @ClassName Zuul_9527_StartSpringCloudApp
+ * @Description
+ * @Author liufei
+ * @Date 2020/8/26 17:29
+ * @Version V1.0
+ **/
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class,args);
+    }
+}
